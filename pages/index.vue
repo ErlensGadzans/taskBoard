71283bea0734a5880aeaca9d1d9c5a47af7f3bd6
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-05-04 17:34:14 +0300

    Func to create new task added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>Hello user</h1>
+<main>
+<h1>Task board</h1>
+<p>Create a list of tasks</p>
+<div class="create-new-task">
+  <input 
+    type="text" 
+    placeholder="Enter new task" 
+    v-model="newTask" 
+    @keypress.enter="addTask"/>
+  <button @click="addTask">Add Task</button>
+</div>
+<div class="tasks">{{$store.state.tasks}}</div>
+
+</main>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data(){
+    return {
+    newTask: ""
+    }
+  },
+  methods:{
+    addTask(){
+      if(this.newTask){
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
 }
 </script>
